bf682332bcf6853ed313e97a9b42be78c7413712
avidrutham <divyamathur89@gmail.com>
2019-02-09 15:55:33 +0530

    add database url

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
 // server.js for the app
-
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const PORT = 3001;
 const cors = require("cors");
 const notebookRoute = require("./notebook.route");
+const mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(process.env.MLAB_URI, {
+    useNewUrlParser: true
+  })
+  .then(
+    () => {
+      console.log("Database is now connected");
+    },
+    err => {
+      console.log("Cannot connect to database + ", err);
+    }
+  );
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use("/notebook", notebookRoute);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to notebook API");
+});
 
 app.listen(PORT, () => {
   console.log("Server is running on PORT: ", PORT);
