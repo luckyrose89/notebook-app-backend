990efa4fb45c61c3cb2efa9932f7e0028d90cd9f
avidrutham <divyamathur89@gmail.com>
2019-02-11 14:54:24 +0530

    notebook get route complete

--- notebook.route.js
+++ notebook.route.js
@@ -4,11 +4,17 @@ const notebookRoute = express.Router();
 let Notebook = require("./notebook.model");
 
 //Define Routes for Notebook
-notebookRoute.get("/notebooks", (req, res) => {
-  res.send("GET WORKS!!");
+notebookRoute.get("/", (req, res) => {
+  Notebook.find((err, notebookData) => {
+    if (err) {
+      console.log("An error occured while fetching db: ", err);
+    } else {
+      res.json(notebookData);
+    }
+  });
 });
 
-notebookRoute.post("/notebooks/add", (req, res) => {
+notebookRoute.post("/add", (req, res) => {
   let notebook = new Notebook(req.body);
   notebook
     .save()
@@ -20,12 +26,22 @@ notebookRoute.post("/notebooks/add", (req, res) => {
     });
 });
 
-notebookRoute.post("/notebooks/:id/edit", (req, res) => {
+notebookRoute.post("/:id/edit", (req, res) => {
   res.send("UPDATE WORKS");
 });
 
-notebookRoute.delete("/notebooks/:id/delete", (req, res) => {
+notebookRoute.delete("/:id/delete", (req, res) => {
   res.send("DELETE WORKS");
+});
+
+// Define routes for Notebook Pages
+
+notebookRoute.get("/:id", (req, res) => {
+  res.send("Notepage get works!!");
+});
+
+notebookRoute.post("/:id/addPage", (req, res) => {
+  res.send("Notebook page add route works!!");
 });
 
 module.exports = notebookRoute;
--- notepage.route.js
+++
@@ -1,3 +0,0 @@
-const express = require("express");
-const notepageRoute = express.Router();
-
