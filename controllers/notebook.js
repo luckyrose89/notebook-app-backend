49a177e99a4d8ab548f5b6e107d3f4b10b483676
avidrutham <divyamathur89@gmail.com>
2019-03-05 22:08:25 +0530

    add controller functions for notebook routes

--- controllers/notebook.js
+++ controllers/notebook.js
@@ -3,8 +3,58 @@
 const Notebook = require("../models/notebook.model");
 
 // Notebook control functions
-const getAll = async (req, res, next) => {};
-const create = async (req, res, next) => {};
-const getOne = async (req, res, next) => {};
-const updateOne = async (req, res, next) => {};
-const deleteOne = async (req, res, next) => {};
+// Get all notebooks
+const getAll = async (req, res, next) => {
+  try {
+    const notebooks = await Notebook.find();
+    res.status(200).json(notebooks);
+  } catch (err) {
+    return next(err);
+  }
+};
+
+// Create a Notebook
+const create = async (req, res, next) => {
+  const notebook = new Notebook(req.body);
+  try {
+    const newNotebook = await notebook.save();
+    res.status(200).json({ notebook: newNotebook });
+  } catch (err) {
+    return next(err);
+  }
+};
+
+// Get a specific notebook
+const getOne = async (req, res, next) => {
+  try {
+    const notebook = await Notebook.findById({ _id: req.params.id });
+    res.status(200).json(notebook);
+  } catch (err) {
+    return next(err);
+  }
+};
+
+// Update a specific notebook
+const updateOne = async (req, res, next) => {
+  try {
+    const updatedNotebook = await Notebook.findById({ _id: req.params.id });
+    updatedNotebook.title = req.body.title;
+    updatedNotebook
+      .save()
+      .then(() => res.json({ message: "Database updated!" }));
+  } catch (err) {
+    return next(err);
+  }
+};
+
+// Delete a specific notebook
+const deleteOne = async (req, res, next) => {
+  try {
+    const deleteNotebook = await Notebook.findOneAndDelete({
+      _id: req.params.id
+    });
+    res.status(200).json(deleteNotebook);
+  } catch (err) {
+    return next(err);
+  }
+};
